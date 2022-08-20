4d0511812fc11d86a90bdcdb6534cd13ea57e2c3
Cazpar <Casti19@student.sdu.dk>
2022-08-20 20:21:43 +0200

    Update ip address for test

--- tests/backend-connection-test.sh
+++ tests/backend-connection-test.sh
@@ -1,5 +1,5 @@
-curl -X POST -H "Content-Type: application/json" -d '{"message":"'"${COMMIT_SHA}"'"}' "${DEV_IP}:8080/backend"
-if curl -s "${DEV_IP}:8080/backend" | grep -q "${COMMIT_SHA}"; then
+curl -X POST -H "Content-Type: application/json" -d '{"message":"'"${COMMIT_SHA}"'"}' "${DEV_IP}:31600/backend"
+if curl -s "${DEV_IP}:316000/backend" | grep -q "${COMMIT_SHA}"; then
   echo "Backend connection test passed"
 else
   echo "Backend connection test failed"
