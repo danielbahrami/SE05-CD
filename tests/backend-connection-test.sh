d674d0ef7bd6e033e8d8239c22914eb7275bb7d0
Cazpar <Casti19@student.sdu.dk>
2022-08-20 22:20:08 +0200

    not sure with bash

--- tests/backend-connection-test.sh
+++ tests/backend-connection-test.sh
@@ -1,7 +1,6 @@
 curl -X POST -H "Content-Type: application/json" -d '{"message":"'"${COMMIT_SHA}"'"}' "${DEV_IP}:31600/backend"
-if curl -s "${DEV_IP}:316000/backend" | grep -q "${COMMIT_SHA}"; then
+if [curl -s "${DEV_IP}:316000/backend" | grep -q "${COMMIT_SHA}"]; then
   echo "Backend connection test passed"
 else
   echo "Backend connection test failed"
-  exit 1
 fi
