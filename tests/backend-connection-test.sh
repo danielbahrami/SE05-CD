c956765760b76eb6490d2a0289016207b6398096
Cazpar <Casti19@student.sdu.dk>
2022-08-20 22:32:46 +0200

    Fix test file

--- tests/backend-connection-test.sh
+++ tests/backend-connection-test.sh
@@ -1,6 +1,6 @@
-curl -X POST -H "Content-Type: application/json" -d '{"message":"'"${COMMIT_SHA}"'"}' "${DEV_IP}:31600/backend"
-if [curl -s "${DEV_IP}:316000/backend" | grep -q "${COMMIT_SHA}"]; then
-  echo "Backend connection test passed"
+curl -X POST -H "Content-Type: application/json" -d '{"message":"'"${COMMIT_SHA}"'"}' "${DEV_IP}:31600"
+if [curl -s "${DEV_IP}:316000" | grep -q "${COMMIT_SHA}"]; then
+  echo "Frontend connection test passed"
 else
-  echo "Backend connection test failed"
+  echo "Frontend connection test failed"
 fi
