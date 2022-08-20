5bf40233980bbdc647adb2f5dd02600d306f9f94
Cazpar <Casti19@student.sdu.dk>
2022-08-20 23:14:34 +0200

    Connection test fixed and working + renaming

--- tests/backend-connection-test.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{"message":"'"${COMMIT_SHA}"'"}' "${DEV_IP}:31600"
-if [curl -s "${DEV_IP}:316000" | grep -q "${COMMIT_SHA}"]; then
-  echo "Frontend connection test passed"
-else
-  echo "Frontend connection test failed"
-fi
