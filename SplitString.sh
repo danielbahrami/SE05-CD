96948046b14a842f5feab2a5727354c74db262bf
Cazpar <Casti19@student.sdu.dk>
2022-08-21 16:54:44 +0200

    Attempt 4

--- SplitString.sh
+++ SplitString.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 str={{ env.DEV_PORT }}
-
-
-#remove everything after / from str
+#remove everything before : from str
 port=${str#*:}
 #remove everything after / from port
 port=${port%%/*}
-
 echo $port
-
-{{ env.DEV_PORT }} == $port
