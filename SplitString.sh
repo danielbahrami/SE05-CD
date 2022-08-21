1ef8772416b57dc03acb99edf74d3d1f0b8ecfaf
Cazpar <Casti19@student.sdu.dk>
2022-08-21 17:27:48 +0200

    Last commit

--- SplitString.sh
+++
@@ -1,7 +0,0 @@
-DEV_PORT=$(kubectl get services -o wide | awk 'FNR == 4 {print $5}')
-str={$DEV_PORT}
-#remove everything before : from str
-port=${str#*:}
-#remove everything after / from port
-port=${port%%/*}
-echo $port
